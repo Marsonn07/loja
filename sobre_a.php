870ade01ec9e4ba2a6449ddf07f21dde4c7879a9
Marsonn07 <rafaelmarson.rafa@gmail.com>
2024-06-27 20:37:29 -0300

    sobre_a.php

---
+++ sobre_a.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja de Roupas - Bem-vindo</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <h1>Renova</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a class="cabecalho_menu" href="inicio_a.php">Início</a></li>
+                <li><a class="cabecalho_menu" href="produtos_a.php">Produtos</a></li>
+                <li><a class="cabecalho_menu_a" href="sobre_a.php">Sobre Nós</a></li>
+                <li><a class="cabecalho_menu" href="contato_a.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+    <section id="sobre">
+        <h2>Sobre Nós</h2>
+        <p>Sobre a Loja de Camisas
+<br>Apaixonados por Camisas!
+<br>
+<br>A Loja de Camisas nasceu da paixão por peças que combinam estilo, qualidade e conforto. Acreditamos que as camisas são mais do que apenas roupas; elas são uma forma de expressar sua personalidade e se sentir confiante em qualquer ocasião.
+<br>
+<br>Nossa Missão:
+<br>Oferecer uma ampla variedade de camisas para todos os gostos e estilos, desde camisas clássicas até as mais modernas e descoladas.
+Selecionar cuidadosamente cada peça, buscando materiais de alta qualidade e acabamentos impecáveis.
+Garantir a satisfação dos nossos clientes, proporcionando uma experiência de compra agradável e segura.
+<br>
+<br>Nossos Valores:
+<br>Qualidade: Priorizamos a qualidade em tudo que fazemos, desde a seleção dos materiais até o atendimento ao cliente.
+Conforto: Acreditamos que as camisas devem ser confortáveis para que você possa se sentir bem e se movimentar com liberdade.
+Estilo: Oferecemos uma variedade de estilos para que você possa encontrar a camisa perfeita para expressar sua personalidade.
+Atendimento ao Cliente: Buscamos oferecer um atendimento personalizado e atencioso, para que você se sinta valorizado e satisfeito com sua compra.
+<br>
+<br>Nossa História:
+<br>A Loja de Camisas foi fundada em 2024 por João Otavio, Kawan Melo e Rafael Marson. Desde então, nos dedicamos a oferecer a melhor experiência de compra para nossos clientes.
+<br>
+<br>Nossa Equipe:
+<br>Nossa equipe é composta por apaixonados por camisas que estão sempre buscando novas tendências e novidades para oferecer aos nossos clientes.
+<br>
+<br>Nossas Lojas:
+<br>Temos lojas físicas em cidades onde a loja tem lojas físicas e também vendemos online em nosso site link para o site da loja.
+<br>
+
+<br>Se tiver alguma dúvida ou sugestão, entre em contato conosco pela pagina de contato ao lado.
+<br>Aguardamos a sua visita!
+
+Palavras-chave: loja de camisas, camisas, moda, estilo, qualidade, conforto, atendimento ao cliente, história, equipe, lojas físicas, online, redes sociais, contato.
+        </p>
+    </section>
+
+    <footer class= "rodape">
+        <p>&copy; 2024 Renova. Todos os direitos reservados.</p>
+    </footer>
+</body>
+</html>
